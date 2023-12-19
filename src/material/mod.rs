1e1733f5f7dde4641ed9798eaffdfb45d43ca6e7
Robin Appelman <robin@icewind.nl>
2023-12-19 01:16:46 +0100

    patch resolving

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -5,8 +5,10 @@ mod worldvertextransition;
 
 pub use lightmappedgeneric::LightMappedGenericMaterial;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 pub use unlitgeneric::UnlitGenericMaterial;
+use vdf_reader::entry::{Entry, Table};
+use vdf_reader::error::UnknownError;
+use vdf_reader::{from_entry, VdfError};
 pub use water::WaterMaterial;
 pub use worldvertextransition::WorldVertexTransitionMaterial;
 
@@ -24,8 +26,49 @@ pub enum Material {
     Patch(PatchMaterial),
 }
 
+impl Material {
+    /// If the material is a patch, apply it to the included material
+    pub fn resolve<E, Loader>(self, loader: Loader) -> Result<Material, E>
+    where
+        Loader: FnOnce(&str) -> Result<String, E>,
+        E: From<VdfError>,
+    {
+        match self {
+            Material::Patch(patch) => patch.resolve(loader),
+            mat => Ok(mat),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PatchMaterial {
     include: String,
-    replace: HashMap<String, String>,
+    replace: Table,
+}
+
+impl PatchMaterial {
+    /// Load the included material and apply the patch
+    pub fn resolve<E, Loader>(&self, loader: Loader) -> Result<Material, E>
+    where
+        Loader: FnOnce(&str) -> Result<String, E>,
+        E: From<VdfError>,
+    {
+        let base = loader(&self.include)?.to_ascii_lowercase();
+        let mut material = Table::load_from_str(&base)?;
+
+        let material_values = match material.iter_mut().next() {
+            Some((_, Entry::Table(table))) => table,
+            _ => {
+                return Err(VdfError::from(UnknownError::from(
+                    "included vdf doesn't look like a material",
+                ))
+                .into())
+            }
+        };
+        for (key, value) in self.replace.iter() {
+            material_values.insert(key.clone(), value.clone());
+        }
+
+        Ok(from_entry(Entry::Table(material))?)
+    }
 }
--- tests/parse.rs
+++ tests/parse.rs
@@ -1,7 +1,17 @@
 use miette::{GraphicalReportHandler, GraphicalTheme};
 use std::fs::read_to_string;
 use test_case::test_case;
+use thiserror::Error;
 use vmt_parser::from_str;
+use vmt_parser::material::Material;
+
+#[derive(Debug, Error)]
+enum LoaderError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Vdf(#[from] vdf_reader::VdfError),
+}
 
 #[test_case("tests/data/concretefloor003.vmt")]
 #[test_case("tests/data/mvm_backpack.vmt")]
@@ -11,7 +21,15 @@ use vmt_parser::from_str;
 fn test_serde(path: &str) {
     let raw = read_to_string(path).unwrap();
     match from_str(&raw) {
-        Ok(result) => insta::assert_ron_snapshot!(path, result),
+        Ok(result) => {
+            insta::assert_ron_snapshot!(path, result);
+            if let Material::Patch(patch) = result {
+                let patched = patch
+                    .resolve(|path| read_to_string(path).map_err(LoaderError::from))
+                    .unwrap();
+                insta::assert_ron_snapshot!(format!("{}_resolved", path), patched);
+            }
+        }
         Err(e) => {
             let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
             let mut out = String::new();
